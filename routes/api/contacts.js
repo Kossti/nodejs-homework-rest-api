61ea55a20214fb65f65dd9ea28e817b9ae679469
Kossti <timoshenko.konstantin@gmail.com>
2023-07-17 23:10:08 +0300

    add: .env, cinfig

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -10,12 +10,12 @@ const router = express.Router();
 
 router.get("/", ctrl.get);
 
-router.get("/:contactId", ctrl.getById);
+// router.get("/:contactId", ctrl.getById);
 
-router.post("/", validateBody(schemas.addSchema), ctrl.add);
+// router.post("/", validateBody(schemas.addSchema), ctrl.add);
 
-router.delete("/:id", ctrl.deleteById);
+// router.delete("/:id", ctrl.deleteById);
 
-router.put("/:id", validateBody(schemas.addSchema), ctrl.updateById);
+// router.put("/:id", validateBody(schemas.addSchema), ctrl.updateById);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,24 +2,16 @@ const mongoose = require("mongoose");
 
 const app = require("./app");
 
-const DB_HOST =
-  "mongodb+srv://Kossti:LUzmBuYvq16JS1k1@cluster0.qq8ihza.mongodb.net/db-contacts?retryWrites=true&w=majority";
+const { DB_HOST, PORT = 3000 } = process.env;
 
 mongoose.set("strictQuery", true);
 
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    app.listen(3000);
+    app.listen(PORT);
   })
   .catch((error) => {
     console.log(error.message);
     process.exit(1);
   });
-
-// app.listen(3000, () => {
-//   console.log("Server running. Use our API on port: 3000");
-// });
-
-// LUzmBuYvq16JS1k1
-// mongodb+srv://Kossti:LUzmBuYvq16JS1k1@cluster0.qq8ihza.mongodb.net/db-contacts?retryWrites=true&w=majority
