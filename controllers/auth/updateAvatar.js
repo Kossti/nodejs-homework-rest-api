155989f0ef614e129a8d998c979c54cdf55aff14
Kossti <timoshenko.konstantin@gmail.com>
2023-08-02 09:31:31 +0300

    fix bag:  - change folder temp to tmp with path and variables

--- controllers/auth/updateAvatar.js
+++ controllers/auth/updateAvatar.js
@@ -9,17 +9,17 @@ const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
 
 const updateAvatar = async (req, res) => {
   const { _id } = req.user;
-  const { path: tempUpload, originalname } = req.file;
+  const { path: tmpUpload, originalname } = req.file;
   const filename = `${_id}_${originalname}`;
   const resultUpload = path.join(avatarsDir, filename);
-  await Jimp.read(tempUpload)
+  await Jimp.read(tmpUpload)
     .then((avatar) => {
-      return avatar.resize(250, 250).write(tempUpload);
+      return avatar.resize(250, 250).write(tmpUpload);
     })
     .catch((err) => {
       console.error(err);
     });
-  await fs.rename(tempUpload, resultUpload);
+  await fs.rename(tmpUpload, resultUpload);
   const avatarURL = path.join("avatars", filename);
   await User.findByIdAndUpdate(_id, { avatarURL });
 
